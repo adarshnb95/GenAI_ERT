c3b1953e7b83d64bd6f0c9a00f1ba0e155b35132
Adarsh Bhandary <adarshnb95@gmail.com>
2025-06-08 23:30:50 -0500

    Added start_app.py, refactored code to fetch ticker

---
+++ ingestion/company_map.py
@@ -0,0 +1,8 @@
+# File: ingestion/company_map.py
+COMPANY_MAP = {
+    "Apple":       "AAPL",
+    "Microsoft":   "MSFT",
+    "Salesforce":  "CRM",
+    "Tesla":       "TSLA",
+    # …add more as you go
+}
