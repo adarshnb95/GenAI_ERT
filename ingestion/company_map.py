52d7d5bb86fb960274d4a05620615935121c8c7a
Adarsh Bhandary <adarshnb95@gmail.com>
2025-06-12 17:03:09 -0500

    Changes for ingesting and processing form N-2

--- ingestion/company_map.py
+++ ingestion/company_map.py
@@ -4,5 +4,6 @@ COMPANY_MAP = {
     "Microsoft":   "MSFT",
     "Salesforce":  "CRM",
     "Tesla":       "TSLA",
+    "Wells Fargo Global Dividend Opportunity Fund":"EOD",
     # …add more as you go
 }
